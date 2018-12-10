fbf815ab242a5d9c0916d274e5cd4df8c16f5d71
Neil Shannon <neil.shannon@cognizant.com>
2018-12-10 10:05:09 -0600

    readability

--- src/main/java/MatrixFlipper.java
+++ src/main/java/MatrixFlipper.java
@@ -5,16 +5,22 @@ public class MatrixFlipper {
 
     /**
      * Flips the diagonals of an NxN matrix of integers
-     * @param matrix a 2d array of equal degree (a square)
+     * @param matrix a 2d array of equal degree (a square or NxN)
      * @return the matrix with the diagonals flipped in place
      */
     public int[][] flipDiagonals(int[][] matrix) {
-        for(int x = 0; x < matrix[0].length; x++){
-            int leftDiagonal = matrix[x][x];
-            int rightDiagonal = matrix[x][matrix[0].length - x - 1];
-            matrix[x][x] = rightDiagonal;
-            matrix[x][matrix[0].length - x - 1] = leftDiagonal;
+        int[] firstRow = matrix[0];
+        int matrixWidth = firstRow.length;
+
+        for(int xCoordinate = 0; xCoordinate < matrixWidth; xCoordinate++){
+            int leftDiagonal = matrix[xCoordinate][xCoordinate];
+            int yCoordinate = matrixWidth - xCoordinate - 1;
+            int rightDiagonal = matrix[xCoordinate][yCoordinate];
+
+            matrix[xCoordinate][xCoordinate] = rightDiagonal;
+            matrix[xCoordinate][yCoordinate] = leftDiagonal;
         }
+
         return matrix;
     }
 
